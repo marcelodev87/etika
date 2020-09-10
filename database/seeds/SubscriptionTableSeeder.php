af57b155ca4a4fd5932ec9e2bb5d637454329d5e
brunokaue <brunokaue@outlook.com.br>
2020-09-10 13:10:51 -0300

    Update SubscriptionTableSeeder.php

--- database/seeds/SubscriptionTableSeeder.php
+++ database/seeds/SubscriptionTableSeeder.php
@@ -12,38 +12,38 @@ class SubscriptionTableSeeder extends Seeder
      */
     public function run()
     {
-        $plans = [
-            [
-                'title' => 'Light',
-                'price' => 120
-            ],
-            [
-                'title' => 'Basic',
-                'price' => 209
-            ],
-            [
-                'title' => 'Plus',
-                'price' => 270
-            ],
-            [
-                'title' => 'Master',
-                'price' => 383
-            ],
-            [
-                'title' => 'Premium',
-                'price' => 520
-            ],
-            [
-                'title' => 'Honorários Contábeis',
-                'price' => 0
-            ],
-        ];
-
-        foreach ($plans as $plan){
-            Subscription::create([
-                'name' => $plan['title'],
-                'price' => $plan['price']
-            ]);
-        }
+//        $plans = [
+//            [
+//                'title' => 'Light',
+//                'price' => 120
+//            ],
+//            [
+//                'title' => 'Basic',
+//                'price' => 209
+//            ],
+//            [
+//                'title' => 'Plus',
+//                'price' => 270
+//            ],
+//            [
+//                'title' => 'Master',
+//                'price' => 383
+//            ],
+//            [
+//                'title' => 'Premium',
+//                'price' => 520
+//            ],
+//            [
+//                'title' => 'Honorários Contábeis',
+//                'price' => 0
+//            ],
+//        ];
+//
+//        foreach ($plans as $plan){
+//            Subscription::create([
+//                'name' => $plan['title'],
+//                'price' => $plan['price']
+//            ]);
+//        }
     }
 }
