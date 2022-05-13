14695703c7b538f65740c724ee869fe73ee90173
Marcelo Dias <marcelo.dev87@gmail.com>
2022-05-13 14:00:01 -0300

    orderBY+datatable

--- resources/views/mandatos/all.blade.php
+++ resources/views/mandatos/all.blade.php
@@ -51,32 +51,11 @@
 @endsection
 @section('script')
     <script type="text/javascript">
-        jQuery.extend(jQuery.fn.dataTableExt.oSort, {
-            "date-uk-pre": function(a) {
-                var ukDatea = a.split('/');
-                return (ukDatea[2] + ukDatea[1] + ukDatea[0]) * 1;
-            },
-
-            "date-uk-asc": function(a, b) {
-                return ((a < b) ? -1 : ((a > b) ? 1 : 0));
-            },
-
-            "date-uk-desc": function(a, b) {
-                return ((a < b) ? 1 : ((a > b) ? -1 : 0));
-            }
-        });
-        $(document).ready(function() {
-            $('#datatable').dataTable({
-                "aoColumns": [
-                    null,
-                    null,
-                    null,
-                    {
-                        "sType": "date-uk"
-                    },
-                    null
-                ]
-            });
+         $("#datatable").dataTable({
+            columnDefs: [
+                {orderable: false, targets: 5}
+            ],
+            language: $datatableBR,
         });
     </script>
 @endsection
